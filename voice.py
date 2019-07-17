dd893b09fedd668ea7f53604ba399cd6332783f7
ndurumo254 <erickndurumo@gmail.com>
2019-07-17 14:59:10 +0300

    first commit

---
+++ voice.py
@@ -0,0 +1,11 @@
+import speech_recognition as sr
+
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("talk now :")
+    audio = r.listen(source)
+    try:
+        text = r.recognize_google(audio)
+        print("You said : {}".format(text))
+    except:
+        print("come up again")
